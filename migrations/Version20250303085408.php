567564e986319da33895f201ee3721bd502e61d7
Marion <marionbailleux07@gmail.com>
2025-03-03 09:54:55 +0100

    first commit

---
+++ migrations/Version20250303085408.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250303085408 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE book (a INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, b VARCHAR(255) NOT NULL, c VARCHAR(255) NOT NULL, d BOOLEAN NOT NULL, e DATETIME DEFAULT NULL, f DATETIME DEFAULT NULL)');
+        $this->addSql('CREATE TABLE user (x INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, y VARCHAR(255) NOT NULL, z CLOB NOT NULL --(DC2Type:array)
+        )');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE book');
+        $this->addSql('DROP TABLE user');
+    }
+}
